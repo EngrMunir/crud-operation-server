7684187a38d4afaaf14d6655252ad308814fcbcd
Munir <smsirajulmonir@gmail.com>
2024-04-21 18:16:22 +0600

    data update api ready

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { useParams } = require('react-router-dom');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -36,11 +37,28 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     })
+
+    app.get('/users/:id', async(req,res)=> {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const user = await userCollection.findOne(query);
+      res.send(user);
+    })
+
+    
     app.post('/users', async(req,res)=>{
         const user = req.body;
         console.log('new user', user)
         const result = await userCollection.insertOne(user);
         res.send(result)
+    })
+
+    app.delete('/users/:id', async(req, res)=>{
+      const id = req.params.id;
+      console.log('please delete from database', id);
+      const query = { _id: new ObjectId(id)}
+      const result = await userCollection.deleteOne(query);
+      res.send(result)
     })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
